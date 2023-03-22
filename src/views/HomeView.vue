d02ff827f62ae40f999d791dca87227fb5927d52
Abubaker Abuelhassan <abubakr.sg@gmail.com>
2023-03-22 17:20:35 +0200

    :tada: starting the project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center h-screen w-full">
+    <h1 class="text-4xl text-blue-600 font-bold capitalize">Blue lock school</h1>
+  </div>
+</template>
